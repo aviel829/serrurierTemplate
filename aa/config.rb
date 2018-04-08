f3c6d7bda188f8694d280d6c64278d7e5057d7b6
aviel829 <aviel829@gmail.com>
2018-04-08 23:33:07 +0300

    Fix ui construct template

---
+++ aa/config.rb
@@ -0,0 +1,15 @@
+
+#preferred_syntax = :sass
+
+http_path = '/'
+http_fonts_path = './'
+http_fonts_dir = 'style/fonts/'
+css_dir = './'
+sass_dir = 'sass/'
+images_dir = 'images/'
+javascripts_dir = 'js/'
+
+preferred_syntax = :scss
+relative_assets = false
+line_comments = false
+output_style = :compressed
